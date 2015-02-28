a44a34cb882940bfb0886e02e9f00cca7e4b7e51
Jason Noble <github+jasonn@jasonnoble.org>
2015-02-27 19:32:19 -0700

    added credit_card.rb

---
+++ credit_card.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# Creating credit card class
+class CreditCard
+  attr_reader :balance
+
+  def initialize
+    @balance = 0.00
+  end
+
+  def charge(amount)
+    @balance + amount
+  end
+end
